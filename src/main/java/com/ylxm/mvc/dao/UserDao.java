44c2c89d70e7805e6ed2fcd703610db43245e5a2
@遥遥领先 <2179853328@qq.com>
2023-10-30 00:14:24 +0800

    Initial commit

---
+++ src/main/java/com/ylxm/mvc/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.ylxm.mvc.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author lhlog351X
+ * com.ylxm.mvc.dao
+ * 2023/10/30 0:13
+ */
+@Repository
+public interface UserDao {
+}
---
+++ src/main/java/com/ylxm/mvc/dao/impl/UserDaoImpl.java
@@ -0,0 +1,12 @@
+package com.ylxm.mvc.dao.impl;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lhlog351X
+ * com.ylxm.mvc.dao.impl
+ * 2023/10/30 0:14
+ */
+@Component
+public class UserDaoImpl {
+}
